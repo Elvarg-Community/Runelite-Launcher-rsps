709a395ab8e915f36c1f9fb2c3c786318a32593d
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2023-05-29 23:43:18 +0100

    Updated to runelite [May 9, 2023]

---
+++ .jdk-versions.sh
@@ -0,0 +1,24 @@
+# windows x64
+WIN64_VERSION=11.0.19+7
+WIN64_CHKSUM=285fc36b709d24b5976ee89fdc4ffbeb9e258bc905e7737a42efccfd2d725b59
+WIN64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-jre_x64_windows_hotspot_11.0.19_7.zip
+# windows x86
+WIN32_VERSION=11.0.19+7
+WIN32_CHKSUM=e0ad2091780fb180009ce2ddf7a711980829623add7a8799a08746c41d625a2c
+WIN32_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-jre_x86-32_windows_hotspot_11.0.19_7.zip
+# mac x64
+MAC_AMD64_VERSION=11.0.19+7
+MAC_AMD64_CHKSUM=87e439b2193e1a2cf1a8782168bba83b558f54e2708f88ea8296184ea2735c89
+MAC_AMD64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-jre_x64_mac_hotspot_11.0.19_7.tar.gz
+# mac aarch64
+MAC_AARCH64_VERSION=17.0.7+7
+MAC_AARCH64_CHKSUM=625d070a297a3c856badbaa5c65adaaa1adb3ea3813363fb8335c47709b69140
+MAC_AARCH64_LINK=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jre_aarch64_mac_hotspot_17.0.7_7.tar.gz
+# linux x64
+LINUX_AMD64_VERSION=11.0.19+7
+LINUX_AMD64_CHKSUM=32dcf760664f93531594b72ce9226e9216567de5705a23c9ff5a77c797948054
+LINUX_AMD64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-jre_x64_linux_hotspot_11.0.19_7.tar.gz
+# linux aarch64
+LINUX_AARCH64_VERSION=11.0.19+7
+LINUX_AARCH64_CHKSUM=1fe4b20d808f393422610818711c728331992a4455eeeb061d3d05b45412771d
+LINUX_AARCH64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.19%2B7/OpenJDK11U-jre_aarch64_linux_hotspot_11.0.19_7.tar.gz
